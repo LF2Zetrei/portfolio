9f822682dc62a89ee1b632a0c6d25cf89521061f
LF2Zetrei <fernandesl@cy-tech.fr>
2026-03-20 15:02:28 +0100

    fix seo

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    formats: ['image/avif', 'image/webp'], // ← compression moderne
-    domains: ['images.unsplash.com'],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
